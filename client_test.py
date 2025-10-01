f615716f56b378d4714bc2ad58105ccfb07dd5dd
larocqueg <portugalgabriel2021@gmail.com>
2025-10-01 21:19:01 +0100

    Trying to reduce the prints

--- client_test.py
+++ client_test.py
@@ -4,7 +4,7 @@ import time
 import numpy as np
 import cv2
 
-client = carla.Client('localhost', 2000)
+client = carla.Client('100.95.17.117', 2000)
 client.set_timeout(5.0)
 time.sleep(2)
 world = client.get_world()
@@ -53,15 +53,22 @@ import random
 # Assume these are initialized globally
 target_speed = 50
 last_signal_time = time.time()
+rand_num = 0;
+print_flag = 1;
 
 def simulate_traffic_signs():
     global target_speed, last_signal_time
     if time.time() - last_signal_time > 3.0:
-        target_speed = random.choice([30, 50, 70])
-        print(f"🚦 New sign detected: limit of {target_speed} km/h")
+        rand_num = random.choice([30, 50, 70])
+        if rand_num != target_speed:
+            print_flag = 1
+            target_speed = rand_num
+        if print_flag == 1:
+            print(f"🚦 New sign detected: limit of {target_speed} km/h")
         last_signal_time = time.time()
     else:
-        print(f"⏳ No new sign. Current speed limit: {target_speed} km/h")
+        if print_flag != 1:
+            print(f"⏳ No new sign. Current speed limit: {target_speed} km/h")
 	
 latest_frame = None
 # process camera img
